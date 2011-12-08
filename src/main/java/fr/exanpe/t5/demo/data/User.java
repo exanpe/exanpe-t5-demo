7307af4db8461c489961674f371f9317eda19b6e
attonnnn <attonnnn@gmail.com>
2011-12-08 19:24:44 +0100

    ListSorter example + Groupinj update

--- src/main/java/fr/exanpe/t5/demo/data/User.java
+++ src/main/java/fr/exanpe/t5/demo/data/User.java
@@ -13,19 +13,47 @@ public class User
 
     private int age;
 
-    public long getId() { return id; }
+    public User()
+    {
+    }
 
-    public void setId(long id) { this.id = id; }
+    public long getId()
+    {
+        return id;
+    }
 
-    public String getFirstName() { return firstName; }
+    public void setId(long id)
+    {
+        this.id = id;
+    }
 
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getFirstName()
+    {
+        return firstName;
+    }
 
-    public String getLastName() { return lastName; }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
 
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getLastName()
+    {
+        return lastName;
+    }
 
-    public int getAge() { return age; }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 
-    public void setAge(int age) { this.age = age; }
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
 }
---
+++ src/main/java/fr/exanpe/t5/demo/pages/components/listSorter/Example1.java
@@ -0,0 +1,48 @@
+package fr.exanpe.t5.demo.pages.components.listSorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+
+import fr.exanpe.t5.demo.data.User;
+import fr.exanpe.t5.lib.annotation.ContextPageReset;
+
+public class Example1
+{
+    @Persist
+    @Property
+    private List<User> list;
+
+    @Property
+    private User element;
+
+    void onActivate()
+    {
+        if (list == null)
+        {
+            cpr();
+        }
+    }
+
+    @ContextPageReset
+    void cpr()
+    {
+        list = new ArrayList<User>();
+        list.add(createUser(1, "John", "Smith", 34));
+        list.add(createUser(1, "Paul", "Carter", 58));
+        list.add(createUser(1, "Simon", "Carpenter", 18));
+        list.add(createUser(1, "Jill", "Valent", 24));
+    }
+
+    private User createUser(long id, String fname, String lname, int age)
+    {
+        User u = new User();
+        u.setId(id);
+        u.setFirstName(fname);
+        u.setLastName(lname);
+        u.setAge(age);
+        return u;
+    }
+}
