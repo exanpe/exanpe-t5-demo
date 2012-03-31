708e0af82302dbf228970c91f1c2daa8d695a7fb
uklance <uklance@gmail.com>
2012-03-31 18:56:09 +0100

    Allow user to use complex tml to define the marker info on a gmap

--- src/main/java/fr/exanpe/t5/demo/pages/components/googleMap/Example2.java
+++ src/main/java/fr/exanpe/t5/demo/pages/components/googleMap/Example2.java
@@ -2,5 +2,7 @@ package fr.exanpe.t5.demo.pages.components.googleMap;
 
 public class Example2
 {
-
+	public String getArcDeTriumphDescription() {
+		return "The Arc de Triomphe is one of the most famous monuments in Paris. It stands in the centre of the Place Charles de Gaulle, at the western end of the Champs-Elysees";
+	}
 }
